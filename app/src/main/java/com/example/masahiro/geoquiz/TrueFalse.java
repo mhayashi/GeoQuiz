564136b029f9873bf1fc240e174b184e7d7c313b
Masahiro Hayashi <hayashi.masahiro@gmail.com>
2014-11-26 15:25:18 +0900

    Add TrueFalse class

---
+++ app/src/main/java/com/example/masahiro/geoquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.example.masahiro.geoquiz;
+
+/**
+ * Created by masahiro on 11/26/14.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
